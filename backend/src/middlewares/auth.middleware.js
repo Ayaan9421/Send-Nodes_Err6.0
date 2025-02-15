b3df70d5a4d80224a8f79387ff448743208f2840
Amin Solkar <aminsolkar5@gmail.com>
2025-02-15 11:23:31 +0530

    Update auth

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -29,3 +29,20 @@ export const protectRoute = async (req, res, next) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+
+export const verifyToken = (req, res, next) => {
+    const token = req.header("Authorization");
+
+    if (!token) {
+        return res.status(403).json({ message: "Access Denied" });
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: "Invalid Token" });
+    }
+};
